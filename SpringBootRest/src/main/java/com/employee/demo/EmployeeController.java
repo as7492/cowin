06c1ded6c4670b0471ea87e8df1e6ef4826cd8c0
Anurag Singh - a0s0gzs <Anurag.Singh0@walmartlabs.com>
2021-05-03 19:53:31 +0530

    Added cowin app

---
+++ SpringBootRest/src/main/java/com/employee/demo/EmployeeController.java
@@ -0,0 +1,24 @@
+package com.employee.demo;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeController {
+	
+	@GetMapping("/cowin/bangalore")
+	public String getAllEmp() {
+		return "Cowin App is up and running";
+	}
+	
+}
